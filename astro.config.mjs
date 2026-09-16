ecde0a2c62ffdd9f000777941b89ebcdda38b2c5
hosotac <hosso.3150@gmail.com>
2026-09-16 20:07:56 +0900

    add PapaLayout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hosota9.github.io',
+    site: "https://hosota9.github.io",
+    image: {
+        layout: "constrained",
+    },
 });
